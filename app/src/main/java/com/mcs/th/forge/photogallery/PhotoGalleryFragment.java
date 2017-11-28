4b4f11148335ca494b9ca41d080fdafac2271b37
fOrgeth <ivdovin@mail.ru>
2017-11-28 22:10:53 +0300

    chapter 25 in progress

--- app/src/main/java/com/mcs/th/forge/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/mcs/th/forge/photogallery/PhotoGalleryFragment.java
@@ -1,10 +1,34 @@
 package com.mcs.th.forge.photogallery;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class PhotoGalleryFragment extends Fragment {
 
-    public static Fragment newInstance() {
+    private RecyclerView mRecyclerView;
+
+    public static PhotoGalleryFragment newInstance() {
         return new PhotoGalleryFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
+        mRecyclerView = v.findViewById(R.id.recycler_view);
+        mRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+        return v;
     }
 }
