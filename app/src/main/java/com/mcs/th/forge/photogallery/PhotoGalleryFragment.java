14effb22fd665f5ec88f714fa6d0e040951a27c2
fOrgeth <ivdovin@mail.ru>
2017-11-28 21:56:19 +0300

    Initial commit

---
+++ app/src/main/java/com/mcs/th/forge/photogallery/PhotoGalleryFragment.java
@@ -0,0 +1,10 @@
+package com.mcs.th.forge.photogallery;
+
+import android.support.v4.app.Fragment;
+
+public class PhotoGalleryFragment extends Fragment {
+
+    public static Fragment newInstance() {
+        return new PhotoGalleryFragment();
+    }
+}
