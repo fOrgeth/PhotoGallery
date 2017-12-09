6f2621823a61e408bde917f0ec64d9a0b6c3bc20
fOrgeth <ivdovin@mail.ru>
2017-12-09 22:02:15 +0300

    !!!Critical commit chapter 26 merged chapter 25 with all challenges

--- app/src/main/java/com/mcs/th/forge/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/mcs/th/forge/photogallery/PhotoGalleryFragment.java
@@ -18,6 +18,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
