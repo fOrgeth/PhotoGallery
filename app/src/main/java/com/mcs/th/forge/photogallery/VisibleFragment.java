0ef1da21383ba7a6d1c10f47aba39050c39aac58
fOrgeth <ivdovin@mail.ru>
2018-01-21 16:42:44 +0300

    chapter 29 finished

--- app/src/main/java/com/mcs/th/forge/photogallery/VisibleFragment.java
+++ app/src/main/java/com/mcs/th/forge/photogallery/VisibleFragment.java
@@ -1,11 +1,12 @@
 package com.mcs.th.forge.photogallery;
 
+import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.support.v4.app.Fragment;
-import android.widget.Toast;
+import android.util.Log;
 
 public abstract class VisibleFragment extends Fragment {
     private static final String TAG = "VisibleFragment";
@@ -27,10 +28,8 @@ public abstract class VisibleFragment extends Fragment {
     private BroadcastReceiver mOnShowNotification = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            Toast.makeText(getActivity(),
-                    "Got a broadcast: " + intent.getAction(),
-                    Toast.LENGTH_LONG)
-                    .show();
+            Log.d(TAG, "Canceling notification");
+            setResultCode(Activity.RESULT_CANCELED);
         }
     };
 }
